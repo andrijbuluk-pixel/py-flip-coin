bb1efdd3bc75a4f3582920d10715158ef11e471e
Andriy <andrijbuluk@gmail.com>
2026-01-28 20:29:08 +0100

    Flip coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import random
+
+
+import matplotlib.pyplot as plt
+
+
+def flip_coin():
+    numbers = {x: 0 for x in range(11)}
+    flip = ["orel", "reshka"]
+
+    for i in range(10000):
+        results = random.choices(flip, k=10)
+        num_heads = results.count("orel")
+        numbers[num_heads] += 1
+
+    for key in numbers:
+        numbers[key] = round(numbers[key] / 10000 * 100, 2)
+
+    return numbers
+
+flip = flip_coin()
+
+x = list(flip.keys())
+y = list(flip.values())
+
+plt.plot(x, y)
+plt.xlabel("Number of series")
+plt.ylabel("Number of drop percentage")
+plt.show()
